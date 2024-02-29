edf95ba2a487ef916d4b88e93307c9446bd70ce0
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-02-29 20:22:03 +0530

    created flight api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,6 +5,8 @@ const airplaneRoutes = require('./airplane-routes')
 const cityRoutes = require('./city-routes')
 const airportRoutes = require('./airport-routes')
 
+const flightRoutes = require('./flight-routes')
+
 const router = express.Router();
 
 router.use('/airplanes', airplaneRoutes);
@@ -12,6 +14,8 @@ router.use('/airplanes', airplaneRoutes);
 router.use('/cities', cityRoutes);
 
 router.use('/airports', airportRoutes);
+
+router.use('/flights', flightRoutes);
 
 router.get('/info', InfoController.info);
 
