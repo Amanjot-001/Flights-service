172076061352dbbb84633c1477b72c1feaea28a0
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-03-11 23:49:15 +0530

    seeder for flights

--- src/seeders/20240310071155-add-Flights.js
+++ src/seeders/20240310071155-add-Flights.js
@@ -7,19 +7,21 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Flights', [
       {
-        flightNumber: '',
-        airplaneId: ,
-        departureAirportId: '',
-        arrivalAirportId: '',
-        arrivalTime: ,
-        departureTime: ,
-        price: ,
-        totalSeats: ,
+        flightNumber: 'UK 808',
+        airplaneId: 1,
+        departureAirportId: 'DEL',
+        arrivalAirportId: 'BLR',
+        arrivalTime: '2024-02-29 23:00:00',
+        departureTime: '2024-02-28 20:00:00',
+        price: 3500,
+        totalSeats: 120,
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ])
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Flight', {[Op.or]: [{flightNumber: ''}, {flightNumber: ''}]})
+    await queryInterface.bulkDelete('Flight', {[Op.or]: [{flightNumber: 'UK 808'}]})
   }
 };
